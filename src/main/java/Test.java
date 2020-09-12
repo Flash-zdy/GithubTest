1286f0cba7dd47346f4cd23c977054ff365f6d09
Flash <20162432027@m.scnu.edu.cn>
2020-09-12 11:00:14 +0800

    Initial commit

---
+++ src/main/java/Test.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("hello ");
+    }
+
+    @org.junit.Test
+    public void test(){
+        System.out.println("hello world");
+    }
+}
